bec94cf518a2c18f366b9b8a6486b2db7a0344f7
UpekshaHerath <upekshadilshan000@gmail.com>
2025-02-18 17:59:50 +0530

    feat: set up the app router for roles

---
+++ front_end/src/app/trader/layout.tsx
@@ -0,0 +1,7 @@
+export default function RootTraderLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
---
+++ front_end/src/app/trader/page.tsx
@@ -0,0 +1,9 @@
+const TradersPage = () => {
+  return (
+    <div>
+      <h1>Traders Page</h1>
+    </div>
+  );
+};
+
+export default TradersPage;
