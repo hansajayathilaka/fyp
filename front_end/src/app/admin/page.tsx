2503c926a407b08185d29aaa8ed684c18e3a6998
UpekshaHerath <upekshadilshan000@gmail.com>
2025-02-19 19:32:42 +0530

    feat: loading skeliton setup

--- front_end/src/app/admin/page.tsx
+++ front_end/src/app/admin/page.tsx
@@ -1,23 +1,17 @@
 import User from "../../models/user";
 
 const AdminPage = async () => {
-  setTimeout(() => {
-    console.log("Admin Page Loaded");
-  }
-  , 5000);
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersJson: User[] = await users.json();
 
   return (
     <div>
-      <h1 className="text-2xl text-gray-300 m-4 mb-8 text-center">
-        Admin Page
-      </h1>
+      <h1 className="text-2xl m-4 mb-8 text-center">Admin Page</h1>
       <ul>
         {usersJson.map((user) => (
           <li
             key={user.id}
-            className="p-4 m-2 bg-black shadow-md rounded-md border-gray-700"
+            className="p-4 m-2 bg-lightBackGround dark:bg-black shadow-md rounded-md border-gray-700"
           >
             {user.name} - {user.email}
           </li>
