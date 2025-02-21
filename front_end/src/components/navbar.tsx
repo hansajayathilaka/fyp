8703a826dcfa1d57330ee87888b27d71d6c7bf85
UpekshaHerath <upekshadilshan000@gmail.com>
2025-02-22 02:59:36 +0530

    feat: nav bar under dev

--- front_end/src/components/navbar.tsx
+++ front_end/src/components/navbar.tsx
@@ -1,42 +1,122 @@
-"use client"
+"use client";
+import { TiThMenu } from "react-icons/ti";
+import Image from "next/image";
+import { IoCloseCircleSharp } from "react-icons/io5";
+import { useState } from "react";
+import { NavbarItemProps } from "@/types/navBar";
+import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
+import Link from "next/link";
 
-import Link from "next/link"
-import { useTheme } from "next-themes"
-import { Moon, Sun } from "lucide-react"
+const NavbarItem = ({ title, classProps }: NavbarItemProps) => {
+  return (
+    <li className={`mx-7 cursor-pointer text-base ${classProps}`}>{title}</li>
+  );
+};
 
-import { Button } from "@/components/ui/button"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-
-export function Navbar() {
-  const { setTheme } = useTheme()
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  const [togleMenu, setTogleMenu] = useState(false);
 
   return (
-    <nav className="sticky top-0 flex items-center justify-between p-2 px-4 bg-lightBackGround shadow-md dark:bg-darkBackGround dark:shadow-darkGray">
-      <div className="text-2xl font-bold text-secondary">
-        <Link href="/">DEIP</Link>
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <Link href="/">
+          {theme === "dark" ? (
+            <Image
+              src={"/icons/logo_dark.png"}
+              alt="logo"
+              width={74}
+              height={82}
+              className="cursor-pointer"
+            />
+          ) : (
+            <Image
+              src={"/icons/logo_light.png"}
+              alt="logo"
+              width={74}
+              height={82}
+              className="cursor-pointer"
+            />
+          )}
+        </Link>
       </div>
-      <div className="flex items-center space-x-4">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon" className="bg-gray-100 dark:bg-darkGray dark:bg-opacity-20">
-              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span className="sr-only">Toggle theme</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <Avatar>
-          <AvatarImage src="/images/avatar.jpeg" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+      <ul className="md:flex hidden list-none flex-row justify-between items-center">
+        {["Home", "About", "Features", "How it Works", "Contact"].map(
+          (item, index) => {
+            return (
+              <NavbarItem
+                title={item}
+                key={item + index}
+                classProps="text-lg"
+              />
+            );
+          }
+        )}
+        <div
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          aria-label="Toggle theme"
+          className="cursor-pointer mr-11"
+        >
+          {theme === "dark" ? (
+            <Image
+              src="/icons/darkModeIcon.png"
+              alt="logo"
+              width={39}
+              height={39}
+            />
+          ) : (
+            <Image
+              src="/icons/lightModeIcon.png"
+              alt="logo"
+              width={39}
+              height={39}
+            />
+          )}
+        </div>
+        <Button className="bg-darkSecondary dark:bg-darkSecondary dark:text-lightBackGround dark:hover:bg-buttonHover hover:bg-buttonHover font-bold text-lg h-[60px] w-[216px] rounded-lg">
+          Connect Wallet
+        </Button>
+      </ul>
+      <div className="flex relative">
+        {togleMenu ? (
+          <IoCloseCircleSharp
+            fontSize="28"
+            className="text-white cursor-pointer md:hidden"
+            onClick={() => setTogleMenu(false)}
+          />
+        ) : (
+          <TiThMenu
+            fontSize="28"
+            className="text-white cursor-pointer md:hidden"
+            onClick={() => setTogleMenu(!togleMenu)}
+          />
+        )}
+        {togleMenu && (
+          <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hiddne list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+            <li className="text-xl w-full my-2">
+              <IoCloseCircleSharp
+                fontSize="28"
+                className="text-white cursor-pointer md:hidden"
+                onClick={() => setTogleMenu(false)}
+              />
+            </li>
+            {["Home", "Services", "Transactions", "About"].map(
+              (item, index) => {
+                return (
+                  <NavbarItem
+                    title={item}
+                    key={item + index}
+                    classProps="my-2 text-lg"
+                  />
+                );
+              }
+            )}
+          </ul>
+        )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
+export default Navbar;
