31d8c56c4b4d3fc5a53307644767fdd4e8c62bba
Hansa Jayathilaka <hansajayathilaka@yahoo.com>
2025-03-17 15:06:15 +0530

    Add docker files to trading system

--- front_end/eslint.config.mjs
+++ front_end/eslint.config.mjs
@@ -7,6 +7,8 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  output: 'standalone',
+  reactStrictMode: true,
 });
 
 const eslintConfig = [
