d422ab9008ceffeee9056de8bffe0631447e52c2
Upeksha Herath <65499669+UpekshaHerath@users.noreply.github.com>
2025-03-17 23:26:30 +0530

    feat: favicon ico add (#24)
    * feat: favicon ico add
    
    * feat: component structure & nav bar changes

---
+++ front_end/src/components/landingPage/AboutSection.tsx
@@ -0,0 +1,7 @@
+export const AboutSection = () => {
+  return (
+    <div>
+      <h1 className="dark:text-white text-black">About Section</h1>
+    </div>
+  );
+};
---
+++ front_end/src/components/landingPage/WhyChooseSection.tsx
@@ -0,0 +1,7 @@
+export const WhyChooseSection = () => {
+  return (
+    <div>
+      <h1 className="dark:text-white text-black">Why Choose Section</h1>
+    </div>
+  );
+};
