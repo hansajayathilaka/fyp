e6e22c1ccae4024e2ba0a403c9720d608d90a33d
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-03-23 15:39:38 +0530

    add background

--- front_end/src/components/landingPage/AboutSection.tsx
+++ front_end/src/components/landingPage/AboutSection.tsx
@@ -1,4 +1,5 @@
 import { Star } from "lucide-react";
+import Image from "next/image";
 
 const Rating = ({ rating }: { rating: number }) => {
   const maxStars = 5;
@@ -9,12 +10,12 @@ const Rating = ({ rating }: { rating: number }) => {
   return (
     <div className="bg-white dark:bg-gray-200text-black dark:text-gray-500 px-3 py-1 rounded-lg flex items-center gap-1 shadow-md 
       sm:px-2 sm:py-1 sm:gap-0.5">
-      
+
       {starsArray.map((type, i) => (
         <Star
           key={i}
           className={type === "full" ? "text-yellow-400" : "text-gray-400"}
-          size={16} 
+          size={16}
           fill={type === "full" ? "yellow" : "gray"}
         />
       ))}
@@ -26,24 +27,36 @@ const Rating = ({ rating }: { rating: number }) => {
 
 export const AboutSection = () => {
   return (
-    <div className="text-black dark:text-white p-6 rounded-xl relative max-w-4xl mx-auto">
-      
-      <h3 className="text-sky-400 text-lg font-semibold">- About</h3>
-      <h2 className="text-black text-2xl font-bold mt-2 dark:text-white">Our Decentralized Equity Investment Platform</h2>
-
-      <div className="mt-4 flex flex-col items-center gap-4 sm:flex-row">
+    <div className="text-black dark:text-white p-6 rounded-xl max-w-8xl mx-auto flex felx-col">
         <div>
-          <p className="text-gray-400 leading-relaxed mt-4 w-md">
-            When you invest with our platform, you’re more than just an investor—you’re a valued stakeholder.
-            As a part of our decentralized ecosystem, you gain access to exclusive equity investment opportunities typically reserved for large institutions.
-            Our platform is built to empower you with secure, transparent, and innovative investment options tailored to your needs.
-          </p>
+          <h3 className="text-sky-400 text-lg font-semibold">- About</h3>
+          <h2 className="text-black text-2xl font-bold mt-2 dark:text-white">Our Decentralized Equity Investment Platform</h2>
+
+          <div className="mt-2 flex flex-col items-center gap-4 sm:flex-row">
+            <div>
+              <p className="text-gray-400 leading-relaxed mt-4">
+                When you invest with our platform, you’re more than just an investor—you’re a valued stakeholder.
+                As a part of our decentralized ecosystem, you gain access to exclusive equity investment opportunities typically reserved for large institutions.
+                Our platform is built to empower you with secure, transparent, and innovative investment options tailored to your needs.
+              </p>
+            </div>
+
+            <div>
+              <Rating rating={4.5} />
+            </div>
+          </div>
         </div>
 
-        <div>
-        <Rating rating={4.5} />
+        <div >
+          <Image
+            src="/images/Group34.png"
+            alt="bg image"
+            width={400}
+            height={600}
+            className="opacity-90"
+          />
         </div>
-      </div>
     </div>
+
   );
 };
