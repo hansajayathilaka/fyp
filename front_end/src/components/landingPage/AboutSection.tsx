b30b3ec7995df5ac04ab468b8fa86a4d559d0605
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-03-23 13:50:12 +0530

    Added mobile responsiveness

--- front_end/src/components/landingPage/AboutSection.tsx
+++ front_end/src/components/landingPage/AboutSection.tsx
@@ -2,12 +2,12 @@ import { Star } from "lucide-react";
 
 export const AboutSection = () => {
   return (
-    <div className="text-black dark:text-white p-6 rounded-xl relative max-w-4xl mx-auto shadow-lg">
+    <div className="text-black dark:text-white p-6 rounded-xl relative max-w-4xl mx-auto">
       
       <h3 className="text-sky-400 text-lg font-semibold">- About</h3>
       <h2 className="text-black text-2xl font-bold mt-2 dark:text-white">Our Decentralized Equity Investment Platform</h2>
 
-      <div className="mt-4 flex items-center gap-4">
+      <div className="mt-4 flex flex-col items-center gap-4 sm:flex-row">
         <div>
           <p className="text-gray-400 leading-relaxed mt-4 w-md">
             When you invest with our platform, you’re more than just an investor—you’re a valued stakeholder.
