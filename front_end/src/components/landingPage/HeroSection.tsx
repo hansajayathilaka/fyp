a6c524ac44f9e5bb57dc2bff643de06b75d02612
Upeksha Herath <65499669+UpekshaHerath@users.noreply.github.com>
2025-03-25 00:47:27 +0530

    27 feat dotted background (#28)
    * feat: applied dotted background
    
    * feat: add flex and height styles to HeroSection

--- front_end/src/components/landingPage/HeroSection.tsx
+++ front_end/src/components/landingPage/HeroSection.tsx
@@ -1,6 +1,6 @@
 export const HeroSection = () => {
   return (
-    <div>
+    <div className="flex-1 h-screen ">
       <h1 className="dark:text-white text-black">Hero Section</h1>
     </div>
   );
