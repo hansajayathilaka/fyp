9860184b1aa93922de30e8e1dcbb41b270bf9862
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-03-27 21:20:38 +0530

    change copyright in footer as appropiate

--- front_end/src/components/footer.tsx
+++ front_end/src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="bottom-0 p-4 mb-0 text-center bg-lightBackGround dark:bg-darkBackGround dark:text-white ">
-      <p>© 2025 DEIP. All rights reserved.</p>
+    <footer className="bottom-0 p-4 mb-0 text-center text-gray-300 bg-lightBackGround dark:bg-darkBackGround dark:text-gray-700 ">
+      <p className="text-sm">© 2025 DEIP. All rights reserved.</p>
     </footer>
   );
 };
