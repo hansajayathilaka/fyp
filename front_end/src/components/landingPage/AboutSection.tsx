dbf3a9ad18d82e423b5ad299e3d744ac7fb712da
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-03-27 09:45:33 +0530

    Formatted the code and remove padding

--- front_end/src/components/landingPage/AboutSection.tsx
+++ front_end/src/components/landingPage/AboutSection.tsx
@@ -8,9 +8,10 @@ const Rating = ({ rating }: { rating: number }) => {
   );
 
   return (
-    <div className="bg-white dark:bg-gray-200 text-black dark:text-gray-500 px-3 py-2 rounded-lg flex items-center gap-1 shadow-md 
-      sm:px-2 sm:py-1 sm:gap-0.5">
-
+    <div
+      className="bg-white dark:bg-gray-200 text-black dark:text-gray-500 px-3 py-2 rounded-lg flex items-center gap-1 shadow-md 
+      sm:px-2 sm:py-1 sm:gap-0.5"
+    >
       {starsArray.map((type, i) => (
         <Star
           key={i}
@@ -20,43 +21,50 @@ const Rating = ({ rating }: { rating: number }) => {
         />
       ))}
 
-      <span className="ml-2 font-semibold sm:ml-1 text-sm sm:text-xs">{rating.toFixed(1)}</span>
+      <span className="ml-2 font-semibold sm:ml-1 text-sm sm:text-xs">
+        {rating.toFixed(1)}
+      </span>
     </div>
   );
 };
 
 export const AboutSection = () => {
   return (
-    <div className="text-black dark:text-white p-6 rounded-xl max-w-8xl mx-auto flex felx-col">
-        <div>
-          <h3 className="text-sky-400 text-lg font-semibold">- About</h3>
-          <h2 className="text-black text-2xl font-bold mt-2 dark:text-white">Our Decentralized Equity Investment Platform</h2>
+    <div className="text-black dark:text-white py-4 rounded-xl max-w-8xl mx-auto flex felx-col">
+      <div>
+        <h3 className="text-sky-400 text-lg font-semibold">- About</h3>
+        <h2 className="text-black text-2xl font-bold mt-2 dark:text-white">
+          Our Decentralized Equity Investment Platform
+        </h2>
 
-          <div className="mt-2 flex flex-col items-center gap-4 sm:flex-row">
-            <div>
-              <p className="text-gray-400 leading-relaxed mt-4 text-md">
-                When you invest with our platform, you’re more than just an investor—you’re a valued stakeholder.
-                As a part of our decentralized ecosystem, you gain access to exclusive equity investment opportunities typically reserved for large institutions.
-                Our platform is built to empower you with secure, transparent, and innovative investment options tailored to your needs.
-              </p>
-            </div>
+        <div className="mt-2 flex flex-col items-center gap-4 sm:flex-row">
+          <div>
+            <p className="text-gray-400 leading-relaxed mt-4 text-md">
+              When you invest with our platform, you’re more than just an
+              investor—you’re a valued stakeholder. As a part of our
+              decentralized ecosystem, you gain access to exclusive equity
+              investment opportunities typically reserved for large
+              institutions. Our platform is built to empower you with secure,
+              transparent, and innovative investment options tailored to your
+              needs.
+            </p>
+          </div>
 
-            <div className="mx-10">
-              <Rating rating={4.5} />
-            </div>
+          <div className="mx-10">
+            <Rating rating={4.5} />
           </div>
         </div>
+      </div>
 
-        <div >
-          <Image
-            src="/images/Group34.png"
-            alt="bg image"
-            width={400}
-            height={600}
-            className="opacity-90"
-          />
-        </div>
+      <div>
+        <Image
+          src="/images/Group34.png"
+          alt="bg image"
+          width={400}
+          height={600}
+          className="opacity-90"
+        />
+      </div>
     </div>
-
   );
 };
