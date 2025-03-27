b85e1cd29e55af11339e2c458bfc35e9e72accf6
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-03-27 12:38:27 +0530

    changed margin size

--- front_end/src/components/landingPage/FooterSection.tsx
+++ front_end/src/components/landingPage/FooterSection.tsx
@@ -3,7 +3,7 @@ import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
 export const FooterSection = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-15 items-center mt-3">
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-15 items-center mt-3 mb-5">
       <div>
         <Image src={"/icons/logo.png"} alt={"logo"} width={60} height={70} />
         <p className="text-sm text-gray-700 dark:text-gray-300 max-w-md">
@@ -14,7 +14,7 @@ export const FooterSection = () => {
       </div>
       <div className="w-3xs"></div>
       <div className="w-md">
-        <h3 className="text-lg font-bold mb-3 mt-3">Contact</h3>
+        <h3 className="text-lg font-bold mb-5 mt-3">Contact</h3>
         <div className="flex items-center gap-4 text-gray-700 dark:text-gray-300 text-sm">
           <FaPhoneAlt />
           <p>619-393-4981 Ext. 101</p>
@@ -24,7 +24,7 @@ export const FooterSection = () => {
           <p>info@deip.com</p>
         </div>
       </div>
-      <div className="flex items-center gap-4 text-gray-700 dark:text-gray-300 text-sm mt-3 w-sm md:justify-end">
+      <div className="flex items-center gap-4 text-gray-700 dark:text-gray-300 text-sm mt-6 w-sm md:justify-end">
         <FaMapMarkerAlt />
         <p>
           University Of Moratuwa
