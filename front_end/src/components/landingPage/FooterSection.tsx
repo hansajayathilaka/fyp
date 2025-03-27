a3303d63b346615df4edde47489ba360aca6c8a7
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-03-27 12:30:22 +0530

    changed colors compatible with dark theme

--- front_end/src/components/landingPage/FooterSection.tsx
+++ front_end/src/components/landingPage/FooterSection.tsx
@@ -6,7 +6,7 @@ export const FooterSection = () => {
     <div className="grid grid-cols-1 md:grid-cols-4 gap-15 items-center mt-3">
       <div>
         <Image src={"/icons/logo.png"} alt={"logo"} width={60} height={70} />
-        <p className="text-sm text-gray-300 max-w-md">
+        <p className="text-sm text-gray-700 dark:text-gray-300 max-w-md">
           A marketplace where investors can securely buy and trade tokenized
           equity from registered companies using blockchain-based semi-fungible
           tokens.
@@ -15,16 +15,16 @@ export const FooterSection = () => {
       <div className="w-3xs"></div>
       <div className="w-md">
         <h3 className="text-lg font-bold mb-3 mt-3">Contact</h3>
-        <div className="flex items-center gap-4 text-gray-300 text-sm">
+        <div className="flex items-center gap-4 text-gray-700 dark:text-gray-300 text-sm">
           <FaPhoneAlt />
           <p>619-393-4981 Ext. 101</p>
         </div>
-        <div className="flex items-center gap-4 text-gray-300 mt-2 text-sm">
+        <div className="flex items-center gap-4 text-gray-700 dark:text-gray-300 mt-2 text-sm">
           <FaEnvelope />
           <p>info@deip.com</p>
         </div>
       </div>
-      <div className="flex items-center gap-4 text-gray-300 text-sm mt-3 w-sm md:justify-end">
+      <div className="flex items-center gap-4 text-gray-700 dark:text-gray-300 text-sm mt-3 w-sm md:justify-end">
         <FaMapMarkerAlt />
         <p>
           University Of Moratuwa
