8888a207c19e1605e99202e107fa3fe029ad2ddc
UpekshaHerath <upekshadilshan000@gmail.com>
2025-03-28 15:43:54 +0530

    fix: update Navbar import path in layout.tsx

--- front_end/src/app/layout.tsx
+++ front_end/src/app/layout.tsx
@@ -1,11 +1,11 @@
-import type React from "react";
+import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Footer from "@/components/footer";
 import { SkeletonTheme } from "react-loading-skeleton";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/navbar/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
