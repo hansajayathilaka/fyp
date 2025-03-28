8b45ea0ca96ebea0ebb20ec67170ec47d307aff8
Binari Dissanayake <91403558+BinuMDX@users.noreply.github.com>
2025-03-28 14:53:30 +0530

    16 about section   landing page (#25)
    * fix the problem in globals.css file
    
    * Finish About me section
    
    * Added mobile responsiveness
    
    * add mapping to rating component
    
    * add background
    
    * change margin size
    
    * Formatted the code and remove padding
    
    * Refactor layout structure and add background image component
    
    * resolve conflict
    
    * Revert "Merge branch 'dev' into 16-about-section---landing-page"
    
    This reverts commit a849708b9df6c251bf643517a3035b3f1e520977, reversing
    changes made to 024bcf4a683ca2611fb378d0422c61e48cf21c85.
    
    * Update layout and AboutSection for improved responsiveness and code clarity
    
    ---------
    
    Co-authored-by: UpekshaHerath <upekshadilshan000@gmail.com>

--- front_end/src/components/landingPage/HeroSection.tsx
+++ front_end/src/components/landingPage/HeroSection.tsx
@@ -1,6 +1,6 @@
 export const HeroSection = () => {
   return (
-    <div className="flex-1 h-screen ">
+    <div>
       <h1 className="dark:text-white text-black">Hero Section</h1>
     </div>
   );
---
+++ front_end/src/components/landingPage/Layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="relative">
+      {/* Background Image */}
+      <div
+        className="absolute top-0 right-[-42px] w-[300px] h-[500px] opacity-30 sm:opacity-90 "
+        style={{
+          backgroundImage: "url('/images/Group34.png')",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      ></div>
+
+      {/* Content */}
+      <div className="px-8 md:px-16 pt-2 min-h-screen relative z-10">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
