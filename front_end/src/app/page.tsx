b811d9dfcc6cbb59780112b7975432f1707111f2
Binari Dissanayake <91403558+BinuMDX@users.noreply.github.com>
2025-03-28 14:53:30 +0530

    16 about section   landing page (#25)
    * fix the problem in globals.css file
    
    * Finish About me section
    
    * Added mobile responsiveness
    
    * add mapping to rating component
    
    * add background
    
    * change margin size
    
    * Formatted the code and remove padding
    
    * Refactor layout structure and add background image component
    
    * resolve conflict
    
    * Revert "Merge branch 'dev' into 16-about-section---landing-page"
    
    This reverts commit a849708b9df6c251bf643517a3035b3f1e520977, reversing
    changes made to 024bcf4a683ca2611fb378d0422c61e48cf21c85.
    
    * Update layout and AboutSection for improved responsiveness and code clarity
    
    ---------
    
    Co-authored-by: UpekshaHerath <upekshadilshan000@gmail.com>

--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -1,16 +1,17 @@
 import { AboutSection } from "@/components/landingPage/AboutSection";
 import { FooterSection } from "@/components/landingPage/FooterSection";
 import { HeroSection } from "@/components/landingPage/HeroSection";
+import Layout from "@/components/landingPage/Layout";
 import { WhyChooseSection } from "@/components/landingPage/WhyChooseSection";
 
 const Page = () => {
   return (
-    <div>
+    <Layout>
       <HeroSection />
       <AboutSection />
       <WhyChooseSection />
       <FooterSection />
-    </div>
+    </Layout>
   )
 };
 
