97d34c70a8fff0ceb9ebb4295a09decd6b6b7708
UpekshaHerath <upekshadilshan000@gmail.com>
2025-03-28 15:44:38 +0530

    feat: implement Navbar component with responsive design and navigation items

--- front_end/src/types/navBar.ts
+++ front_end/src/types/navBar.ts
@@ -1,4 +1,18 @@
-export interface NavbarItemProps {
+export interface NavBarItemType {
   title: string;
   classProps?: string;
+  url: string;
+}
+
+
+export interface LightDarkSwitcherButtonProps {
+  styles?: string;
+}
+
+export interface MobileNavBarProps {
+  setTogleMenu: (value: boolean) => void;
+}
+
+export interface ConnectWalletButtonType {
+  styles?: string;
 }
