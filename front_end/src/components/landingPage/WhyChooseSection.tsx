91de0c19e6d31a9c69b24408cd79f0dab7066e33
Ravindu Karunaweera <94894276+RavinduLK@users.noreply.github.com>
2025-04-29 18:47:48 +0530

    changed border in cards

--- front_end/src/components/landingPage/WhyChooseSection.tsx
+++ front_end/src/components/landingPage/WhyChooseSection.tsx
@@ -3,7 +3,7 @@ import { FaLock, FaCheckCircle, FaChartLine, FaGavel } from "react-icons/fa";
 export const WhyChooseSection = () => {
   return (
     <div id="features">
-    <section className="py-3 px-3 mt-20 md:px-20 bg-transparent text-black dark:text-white">
+    <section className="py-3 px-3 mt-20 bg-transparent text-black dark:text-white">
       <div className="text-center mb-12">
         <h2 className="text-2xl md:text-4xl font-bold">
           Why Choose Our Decentralized Equity Investment Platform?
@@ -11,12 +11,12 @@ export const WhyChooseSection = () => {
         <p className="mt-4 max-w-2xl mx-auto text-sm text-gray-400">
           Invest securely in tokenized equity with blockchain-powered transparency. Our platform ensures seamless trading, regulatory compliance, and exclusive access to investment opportunities—empowering both companies and investors with a decentralized, trustless system.
         </p>
-        <div className="mt-2 w-16 h-1 bg-[#00B8E4] mx-auto rounded-full" />
+        <div className="mt-2 w-32 h-1 bg-[#00B8E4] mx-auto rounded-full" />
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* Card 1 */}
-        <div className="flex items-start gap-4 border p-6 rounded-lg shadow-sm">
+        <div className="flex items-start gap-4 rounded-lg shadow-sm mb-3 p-6">
           <div className="text-[#00B8E4] text-3xl">
             <FaLock />
           </div>
@@ -29,7 +29,7 @@ export const WhyChooseSection = () => {
         </div>
 
         {/* Card 2 */}
-        <div className="flex items-start gap-4 border p-6 rounded-lg shadow-sm">
+        <div className="flex items-start gap-4 rounded-lg shadow-sm mb-3 p-6">
           <div className="text-[#00B8E4] text-3xl">
             <FaCheckCircle />
           </div>
@@ -42,7 +42,7 @@ export const WhyChooseSection = () => {
         </div>
 
         {/* Card 3 */}
-        <div className="flex items-start gap-4 border p-6 rounded-lg shadow-sm">
+        <div className="flex items-start gap-4 rounded-lg shadow-sm mb-3 p-6">
           <div className="text-[#00B8E4] text-3xl">
             <FaChartLine />
           </div>
@@ -55,7 +55,7 @@ export const WhyChooseSection = () => {
         </div>
 
         {/* Card 4 */}
-        <div className="flex items-start gap-4 border p-6 rounded-lg shadow-sm">
+        <div className="flex items-start gap-4 rounded-lg shadow-sm mb-3 p-6">
           <div className="text-[#00B8E4] text-3xl">
             <FaGavel />
           </div>
