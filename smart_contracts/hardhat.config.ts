3b71c467b81ced58a7458672bbbe90c9fcb142f1
Upeksha Herath <upeksha.h@informaticsint.com>
2025-08-02 13:54:58 +0530

    fix: config the chain id to env file

--- smart_contracts/hardhat.config.ts
+++ smart_contracts/hardhat.config.ts
@@ -8,11 +8,15 @@ dotenv.config();
 // Ensure private key is available
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const HEDERA_TESTNET_URL = process.env.HEDERA_TESTNET_URL;
+const CHAIN_ID = process.env.CHAIN_ID;
 if (!PRIVATE_KEY) {
   throw new Error("Please set your PRIVATE_KEY in a .env file");
 }
 if (!HEDERA_TESTNET_URL) {
   throw new Error("Please set your HEDERA_TESTNET_URL in a .env file");
+}
+if (!CHAIN_ID) {
+  throw new Error("Please set your CHAIN_ID in a .env file");
 }
 
 const config: HardhatUserConfig = {
@@ -34,7 +38,7 @@ const config: HardhatUserConfig = {
     // Hedera testnet configuration
     hederaTestnet: {
       url: HEDERA_TESTNET_URL,
-      chainId: 57054,
+      chainId: parseInt(CHAIN_ID, 10),
       accounts: [PRIVATE_KEY],
       gas: 300000,
       gasPrice: 360000000000, // 360 gwei
